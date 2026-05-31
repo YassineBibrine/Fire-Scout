b21808a62e1d704b4f133ac5c49022d00a4da6ec
ISMAIL HIRICH <187509650+ISMHH06@users.noreply.github.com>
2026-06-01 00:36:48 +0100

    refactor: Remove redundant imports in detection_robot.launch.py

--- src/response/launch/detection_robot.launch.py
+++ src/response/launch/detection_robot.launch.py
@@ -5,10 +5,6 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-from launch_ros.actions import Node
-
-from ament_index_python.packages import get_package_share_directory
-import os
 
 
 def generate_launch_description():
